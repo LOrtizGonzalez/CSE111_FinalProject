6b48cd1b011b81ef8e101996e43c9d32d69c47ee
lortiz-gonzalez <lortiz-gonzalez@ucmerced.edu>
2022-12-04 22:22:59 -0800

    Starting the queries for web page

--- queries.sql
+++ queries.sql
@@ -6,38 +6,44 @@ SELECT * FROM transactions;
 SELECT * FROM warehouse;
 
 --1) Returns vehicle when ONLY make is entered
-SELECT *--a_VIN, a_model, a_type, a_year, a_condition, a_color, a_price
-FROM automobile
-WHERE a_make = 'Chevrolet';
+SELECT * FROM automobile WHERE a_make = 'Chevrolet';
 --Returns vehicle when ONLY year is entered
-SELECT * 
-FROM automobile
-WHERE a_year = 2022;
+SELECT *  FROM automobile WHERE a_year = 2022;
 --Returns vehicle when ONLY condition is entered
-SELECT *
-FROM automobile
-WHERE a_condition = "New";
+SELECT * FROM automobile WHERE a_condition = "New";
 --Returns vehicle when ONLY type is entered
-SELECT * 
-FROM automobile
-WHERE a_type = "Sedan";
+SELECT *  FROM automobile WHERE a_type = "Sedan";
+--Returns vehicle when ONLY price is entered
+SELECT * FROM automobile WHERE a_price = 25000;
 
---2) Returns vehicle when make AND model are entered
-SELECT *
-FROM automobile
-WHERE a_make = 'Chevrolet'
-    AND a_model = 'Tahoe';
---Returns vehicle when ONLY model is entered
-SELECT *
-FROM automobile
-WHERE a_model = 'Camry';
---Returns for year entered
-SELECT *
-FROM automobile
-WHERE a_year = 2022;
+--2) make, model, year, condition, type, price
+--Returns vehicle when make AND model are entered
+SELECT * FROM automobile
+WHERE a_make = 'Chevrolet' AND a_model = 'Tahoe';
 --Returns for make AND year
-SELECT *
-FROM automobile
-WHERE a_make = 'Ford'
-    AND a_year = 2021;
-
+SELECT * FROM automobile
+WHERE a_make = 'Ford' AND a_year = 2021;
+--Returns vehicle when make AND condition
+SELECT * FROM automobile
+WHERE a_make = 'Ford' AND a_condition = 'Used';
+--Returns vehicle when make AND type entered
+SELECT * FROM automobile
+WHERE a_make = 'Toyota' AND a_type = 'Sedan';
+--Returns vehicles when make AND price entered
+SELECT * FROM automobile
+WHERE a_make = 'Dodge' AND a_price = 20000;
+--year AND condition (COULD BE REDUNDANT; MIGHT REMOVE LATER)
+SELECT * FROM automobile 
+WHERE a_year = 2021 AND a_condition = 'Used';
+--year AND  type
+SELECT * FROM automobile
+WHERE a_year = 2021 AND a_type = 'SUV';
+--year AND price (REDUNDANT; COULD DELETE LATER)
+SELECT * FROM automobile
+WHERE a_year = 2019 AND a_price = 18000;
+--condition AND type
+SELECT * FROM automobile
+WHERE a_condition = 'Used' AND a_type = 'SUV';
+--type AND price
+SELECT * FROM automobile
+WHERE a_type = 'SUV' AND a_price = 30000;
